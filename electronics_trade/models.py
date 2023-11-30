4e064496d9a8d4f0ca190153b566475267c1f3d0
vovai <vova.izmodenov@mail.ru>
2023-11-30 21:09:21 +0500

    Создал модели + тесты к модели с методами для теста методов (Выполнение пунктов задания 1 и 2)

--- electronics_trade/models.py
+++ electronics_trade/models.py
@@ -1,18 +1,94 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 NULLABLE = {'null': True, 'blank': True}
 
 
 class Participant(models.Model):
-    """Модель участника сети"""
-    pass
+    """
+    Модель участника сети
+    Участниками могут быть заводы, розничные сети и ИП;
+    Иерархия поставщиков по уровням начиная от 0, у завода всегда уровень равен 0.
+    """
+    PARTICIPANT_TYPES = (
+        ('Завод', 'Завод'),
+        ('Розничная сеть', 'Розничная сеть'),
+        ('Индивидуальный предприниматель', 'Индивидуальный предприниматель'),
+    )
+    name = models.CharField(max_length=100, verbose_name='Название участника')
+    participant_type = models.CharField(max_length=40, choices=PARTICIPANT_TYPES, verbose_name='Тип участника')
+    supplier = models.ForeignKey('self', **NULLABLE, on_delete=models.SET_NULL, verbose_name='Поставщик')
+    debt_to_supplier = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Задолженность поставщику',
+                                           **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    level = models.IntegerField(verbose_name='Уровень в иерархии', editable=False)
+
+    class Meta:
+        """
+        Для отображения в админ-панели
+        """
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+    def __str__(self):
+        return f'Участник: {self.name}'
+
+    def save(self, *args, **kwargs):
+        """
+        Автоматическое вычисление и установка уровня участника по следующему принципу:
+        Если есть поставщик, то уровень участника на 1 выше чем у его поставщика
+        """
+        if self.supplier:
+            self.level = self.supplier.level + 1
+        else:
+            self.level = 0
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        """
+        Проверка на то, что у завода не может быть уровень отличный от 0
+        Также только завод может иметь уровень 0
+        """
+        if self.participant_type == 'Завод' and self.level != 0:
+            raise ValidationError("Завод может иметь только уровень 0")
+        elif self.participant_type in ['Розничная сеть', 'Индивидуальный предприниматель'] and self.level == 0:
+            raise ValidationError("Только завод может иметь уровень 0")
+        super().clean()
 
 
 class Contacts(models.Model):
-    """Модель контактов участника"""
-    pass
+    """
+    Модель контактов участника
+    """
+    participant = models.OneToOneField(Participant, on_delete=models.CASCADE, verbose_name='Участник')
+    email = models.EmailField(**NULLABLE, verbose_name='Email')
+    country = models.CharField(max_length=100, **NULLABLE, verbose_name='Страна')
+    city = models.CharField(max_length=100, **NULLABLE, verbose_name='Город')
+    street = models.CharField(max_length=100, **NULLABLE, verbose_name='Улица')
+    house_number = models.CharField(max_length=20, **NULLABLE, verbose_name='Номер дома')
+
+    class Meta:
+
+        verbose_name = 'Контакты'
+        verbose_name_plural = 'Контакты'
+
+    def __str__(self):
+        return f'Контакты {self.participant}'
 
 
 class Product(models.Model):
-    """Модель продуктов участника"""
-    pass
+    """
+    Модель продуктов участника
+    """
+    owner = models.ForeignKey(Participant, on_delete=models.CASCADE, verbose_name='Владелец')
+    title = models.CharField(max_length=100, verbose_name='Название')
+    model = models.CharField(max_length=100, verbose_name='Модель')
+    release_date = models.DateField(verbose_name='Дата выхода продукта на рынок')
+
+    class Meta:
+
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'Продукт: {self.title}, {self.owner}'
---
+++ electronics_trade/tests/test_participant.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+from electronics_trade.models import Participant
+from rest_framework.exceptions import ValidationError
+
+
+class ParticipantTest(TestCase):
+    """
+    Тесты для модели участника и её методов
+    """
+    def setUp(self):
+        """
+        Создаём экземпляры завода, розничной сети и ИП
+        """
+        self.factory = Participant.objects.create(
+            name='Factory',
+            participant_type='Завод',
+            level=0
+        )
+        self.retail = Participant.objects.create(
+            name='Retail',
+            participant_type='Розничная сеть',
+            supplier=self.factory
+        )
+        self.individual = Participant.objects.create(
+            name='Individual',
+            participant_type='Индивидуальный предприниматель',
+            supplier=self.retail
+        )
+
+    def test_str_representation(self):
+        """
+        Проверяет, что метод __str__ возвращает правильное представление участника
+        """
+        self.assertEqual(str(self.factory), 'Участник: Factory')
+        self.assertEqual(str(self.retail), 'Участник: Retail')
+        self.assertEqual(str(self.individual), 'Участник: Individual')
+
+    def test_save_method(self):
+        """
+        Проверяет, что метод save правильно вычисляет и устанавливает уровень участника
+        """
+        self.assertEqual(self.factory.level, 0)
+        self.assertEqual(self.retail.level, 1)
+        self.assertEqual(self.individual.level, 2)
+
+    def test_clean_method(self):
+        """
+        Проверяет, что метод clean работает правильно
+        """
+        invalid_participants = [
+            Participant(name='Invalid_factory', participant_type='Завод', level=1),  # у завода level не может быть > 0
+            Participant(name='Invalid_retail', participant_type='Розничная сеть', level=0)  # только у завода level = 0
+        ]
+        for participant in invalid_participants:
+            with self.assertRaises(ValidationError):
+                participant.clean()
